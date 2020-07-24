c7d74dedd917e5226c9d61f5c319499897c9010d
Ariazm0 <1106030429@qq.com>
2020-07-24 22:55:45 +0800

    Initial commit

---
+++ src/entity/Music.java
@@ -0,0 +1,77 @@
+package entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Ariazm
+ * Date: 2020-07-24
+ * Time: 22:49
+ */
+public class Music {
+    private int musicId;
+    private String title;
+    private String singer;
+    private String url;
+    private String time;
+    private int userId;
+
+    public int getMusicId() {
+        return musicId;
+    }
+
+    public void setMusicId(int musicId) {
+        this.musicId = musicId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "Music{" +
+                "musicId=" + musicId +
+                ", title='" + title + '\'' +
+                ", singer='" + singer + '\'' +
+                ", url='" + url + '\'' +
+                ", time='" + time + '\'' +
+                ", userId=" + userId +
+                '}';
+    }
+}
